0535003177c7149c825b928e2d1a0bcc9656619f
Flawxy <58270365+Flawxy@users.noreply.github.com>
2020-01-08 18:04:54 +0100

    init

--- commands/fs.js
+++ commands/fs.js
@@ -11,6 +11,23 @@ module.exports = {
         if (!args[1]) return message.channel.send(`\nLa bonne syntaxe est : \`${prefix}${this.name} ${this.usage}\``);
         if (args[0].toLowerCase() === 'php' && args[1]) {
             //Tout ce qui concerne la documentation PHP
+
+            const rp = require('request-promise');
+            const url = `https://www.php.net/${args[1]}`;
+
+            rp(url)
+                .then(function(html){
+                    //success!
+                    console.log(html);
+                })
+                .catch(function(err){
+                    //handle error
+                });
+
+
+
+
+
             return message.reply(`<https://www.php.net/${args[1]}>`);
         }
 
