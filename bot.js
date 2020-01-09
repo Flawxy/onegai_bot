47a679020f81f4daead27105dc4630ab9510f3bf
Flawxy <58270365+Flawxy@users.noreply.github.com>
2020-01-09 10:09:15 +0100

    Added aliases for commands

--- bot.js
+++ bot.js
@@ -44,11 +44,10 @@ client.on('message', message => {
     const args = message.content.slice(prefix.length).split(/ +/);
     const commandName = args.shift().toLowerCase();
 
-    if (!client.commands.has(commandName)) return;
+    const command = client.commands.get(commandName)
+                    || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
 
-
-
-    const command = client.commands.get(commandName);
+    if (!command) return;
 
     if (command.guildOnly && message.channel.type !== 'text') {
         return message.reply('Je ne peux pas utiliser cette commande dans les messages privés !');
